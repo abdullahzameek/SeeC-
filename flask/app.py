a8ed24805204125fbdd3d47bd3db85315caed1a4
Abdullah Zameek <arz268@nyu.edu>
2019-09-07 01:45:12 -0400

    capital one works

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,46 @@
 from flask import Flask
-app = Flask(__name__)
+import requests
+import json
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+capitalOneAPIKey = 'f990b904d48c2277e4b75f9ddd8ed3c9'
+app = Flask(__name__)
+customerID = ''
+
+################################### Capital One Related Stuff #######################################
+
+def getAllCustomers():
+    url = 'http://api.reimaginebanking.com/customers?key=f990b904d48c2277e4b75f9ddd8ed3c9'
+    response = requests.get(url)
+    print(response.text)
+
+    if response.status_code == 404:
+	    print('oopsy whoopsy we made a fucky wucky')
+    
+def getNewCustID():
+    pass
+
+def createCustomerOne():
+    pass
+
+def addCustBalanceOne():
+    pass
+
+def subCustBalance():
+    pass
+
+def viewCustBalance():
+    pass
+
+
+##################################### Firebase Stuff ###################################################
+
+def createCustomerFB():
+    pass
+
+def addCustMiles():
+    pass
+
+
 
 if __name__ == "__main__":
-app.run()
+    getAllCustomers()
