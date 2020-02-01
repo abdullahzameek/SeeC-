71f6f7b0876fb5998063d95a467d4a5cdb99244c
Abdullah Zameek <arz268@nyu.edu>
2020-02-01 12:58:29 +0400

    added

--- flask/test.py
+++ flask/test.py
@@ -1,11 +1,11 @@
 import requests, json
 
-url = "https://seecseec.appspot.com//make-purchase"
-url1 = "http://127.0.0.1:5000/get-customer-data"
-url2 = "http://127.0.0.1:5000/get-coupons"
-url3 = "http://127.0.0.1:5000/get-customer-coupons"
-url4 = "http://127.0.0.1:5000/add-balance"
-url5 = "http://127.0.0.1:5000/make-purchase"
+# url = "https://seecseec.appspot.com//make-purchase"
+# url1 = "http://127.0.0.1:5000/get-customer-data"
+# url2 = "http://127.0.0.1:5000/get-coupons"
+# url3 = "http://127.0.0.1:5000/get-customer-coupons"
+# url4 = "http://127.0.0.1:5000/add-balance"
+# url5 = "http://127.0.0.1:5000/make-purchase"
 
 payload8 = {
     "cust_ID": "5d74ef393c8c2216c9fcaf3c",
